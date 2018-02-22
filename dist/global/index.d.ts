6ebdefab8e8ba450c69dcf99af93bc1830e1db8d
pipeline <pipeline@syncfusion.com>
2018-02-22 03:09:16 +0000

    v16.1.24-preview is released

---
+++ dist/global/index.d.ts
@@ -0,0 +1,25 @@
+/*!
+*  filename: index.d.ts
+*  version : 15.4.27
+*  Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+*  Use of this code is subject to the terms of our license.
+*  A copy of the current license can be obtained at any time by e-mailing
+*  licensing@syncfusion.com. Any infringement will be prosecuted under
+*  applicable laws. 
+*/
+
+import * as _charts from '@syncfusion/ej2-charts';
+import * as _base from '@syncfusion/ej2-base';
+import * as _data from '@syncfusion/ej2-data';
+import * as _pdfexport from '@syncfusion/ej2-pdf-export';
+import * as _fileutils from '@syncfusion/ej2-file-utils';
+import * as _compression from '@syncfusion/ej2-compression';
+
+export declare namespace ej {
+    const charts: typeof _charts;
+    const base: typeof _base;
+    const data: typeof _data;
+    const pdfexport: typeof _pdfexport;
+    const fileutils: typeof _fileutils;
+    const compression: typeof _compression;
+}
